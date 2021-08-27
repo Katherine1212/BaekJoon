3a100deb1e48eac1cf1c0d69ff8eed8d7b304871
Katherine1212 <katherinekim971212@gmail.com>
2021-08-27 11:04:01 +0900

    버전 2 런타임 에러(Array Index Out of Bounds) 해결.

--- src/baekjoon/Bj10870ver2.java
+++ src/baekjoon/Bj10870ver2.java
@@ -7,10 +7,14 @@ public class Bj10870ver2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		
 		int [] Fibonacci= new int[n+1];
+		
 		for(int i= 0; i< Fibonacci.length; i++) {
-			if(n==0) Fibonacci[0]=0;
+			// 0,1 은 0,1로 리턴
+			if(i==0) Fibonacci[0]=0;
 			else if(i==1) Fibonacci[1]=1;
+			// 0,1 제외 피보나치 구하기
 			else Fibonacci[i]= Fibonacci[i-1]+Fibonacci[i-2];
 		}
 		System.out.println(Fibonacci[n]);
