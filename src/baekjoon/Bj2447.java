d3fc47f8ab86547c6de2005096f747c3028dfbc4
Katherine1212 <katherinekim971212@gmail.com>
2021-08-28 23:32:06 +0900

    백준 별찍기 -10

---
+++ src/baekjoon/Bj2447.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bj2447 {
+	static StringBuilder sb= new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int x=0; x<n; x++) {
+			for(int y=0; y<n; y++) {
+				print(x, y);
+			}
+			sb.append("\n");
+		}
+	}
+	
+	static void print(int x, int y) {
+		while(true) {
+			if(x==0) break;
+			if(x%3 == 1&& y%3 ==1) {
+				sb.append(" ");
+				return ;
+			}
+			x/=3;
+			y/=3;
+		}
+		sb.append("*");
+	}
+
+}
